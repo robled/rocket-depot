16f8f574b94deb6cd021958b936455279ec9eee0
robled <robled@gmail.com>
2013-07-25 16:28:11 -0400

    attempt at better PEP 8 compliance. See issue #5

--- rdesktop-opener.py
+++ rdesktop-opener.py
@@ -3,148 +3,157 @@
 # Originally written by Cory Wright
 # http://projects.standblue.net/software/#rdesktop-open
 
+import os
+import string
 from Tkinter import *
-import os, string
 
 if os.environ.has_key('USER'):
     user = os.environ['USER']
 else:
-    user = "username"
+    user = 'username'
 
-options = { "host"          : "host.example.com",
-            "user"          : user,
-            "resolution"    : "1024x768",
-            "pass"          : "",
-            "fullscreen"    : 0,
-            "grabkeyboard"  : 0,
-            "homeshare"     : 0 }
+options = { 'host'          : 'host.example.com',
+            'user'          : 'user',
+            'resolution'    : '1024x768',
+            'pass'          : '',
+            'fullscreen'    : 0,
+            'grabkeyboard'  : 0,
+            'homeshare'     : 0 }
 
-optlist = ("host","user","resolution","fullscreen","grabkeyboard","homeshare")
+optlist = ('host', 'user', 'resolution', 'fullscreen', 'grabkeyboard', 'homeshare')
 
-configfile = "%s/.rdesktop-opener" % os.environ['HOME']
+configfile = '%s/.rdesktop-opener' % os.environ['HOME']
 
-def popup_alert(title,textmsg):
+def popup_alert(title, textmsg):
     alert = Tk(className='rdesktop-opener')
-    alert.title("Notice: "+title)
-    Label(alert,text="Notice: "+textmsg).pack()
+    alert.title('Notice: '+title)
+    Label(alert, text='Notice: '+textmsg).pack()
     alert.mainloop()
 
 def save_conf():
-    # host, user name, resolution, fullscreen (0,1), grab keyboard (0,1), homeshare (0,1)
-    conf = open(configfile,"w")
+    # host, user name, resolution, fullscreen (0,1), grab keyboard (0,1),
+    # homeshare (0,1)
+    conf = open(configfile, 'w')
     geometry = string.strip(textGeometry.get())
-    ofline = "%s,%s,%s," % (textHost.get(),textUsername.get(),geometry)
-    ofline = ofline + "%s,%s,%s\n" % (varFs.get(),varGrabKeyboard.get(),varHomeShare.get())
+    ofline = '%s,%s,%s,' % (textHost.get(), textUsername.get(), geometry)
+    ofline = ofline + '%s,%s,%s\n' % (
+        varFs.get(), varGrabKeyboard.get(), varHomeShare.get())
     conf.write(ofline)
     conf.close()
 
 def open_url(url):
-    os.spawnvp("P_NOWAIT","x-www-browser",('x-www-browser',url))
+    os.spawnvp('P_NOWAIT', 'x-www-browser',('x-www-browser' ,url))
     return
 
 def open_rdesktop_site():
-    open_url("http://www.rdesktop.org/")
+    open_url('http://www.rdesktop.org/')
     return
 
 def open_rdo_site():
-    open_url("http://projects.standblue.net/software/")
+    open_url('http://projects.standblue.net/software/')
     return
 
 def open_rdoer_site():
-    open_url("https://github.com/robled/rdesktop-opener/")
+    open_url('https://github.com/robled/rdesktop-opener/')
     return
 
 def run_rdesktop():
     params = ['rdesktop']
 
-    if(textHost.get() == ""):
-        popup_alert("No Host","No Host or IP Address Given")
+    if textHost.get() == '':
+        popup_alert('No Host', 'No Host or IP Address Given')
         return
-    if(textUsername.get() != ""):
-        params.append("-u")
-        params.append("%s" % string.strip(textUsername.get()))
-    if(textPassword.get() != ""):
-        params.append("-p")
-        params.append("%s" % string.strip(textPassword.get()))
-    if(textGeometry.get() != ""):
-        params.append("-g")
-        params.append("%s" % string.strip(textGeometry.get()))
-    if(varFs.get() == 1):
-        params.append("-f")
-    if(varGrabKeyboard.get() == 0):
-        params.append("-K")
-    if(varHomeShare.get() == 1):
-        params.append("-r")
+    if textUsername.get() != '':
+        params.append('-u')
+        params.append('%s' % string.strip(textUsername.get()))
+    if textPassword.get() != '':
+        params.append('-p')
+        params.append('%s' % string.strip(textPassword.get()))
+    if textGeometry.get() != '':
+        params.append('-g')
+        params.append('%s' % string.strip(textGeometry.get()))
+    if varFs.get() == 1:
+        params.append('-f')
+    if varGrabKeyboard.get() == 0:
+        params.append('-K')
+    if varHomeShare.get() == 1:
+        params.append('-r')
         params.append('disk:home=' + os.environ['HOME'])
-    if(textHost.get() != ""):
-        params.append("%s" % string.strip(textHost.get()))
+    if textHost.get() != '':
+        params.append('%s' % string.strip(textHost.get()))
 
-    print params
     os.spawnvp(os.P_NOWAIT, params[0], params)
     return
 
 def print_options():
-    print "-- default options --"
+    print '-- default options --'
     for i in options.keys():
-        print i, " => ", options[i]
-    print "-- currently selected options --"
-    print "host => " + textHost.get()
-    print "user => " + textUsername.get()
-    print "resolution => " + textGeometry.get()
-    print "pass => " + textPassword.get()
-    print "fullscreen => " + str(varFs.get())
-    print "grabkeyboard => " + str(varGrabKeyboard.get())
-    print "homeshare => " + str(varHomeShare.get())
+        print i, ' => ', options[i]
+    print '-- currently selected options --'
+    print 'host => ' + textHost.get()
+    print 'user => ' + textUsername.get()
+    print 'resolution => ' + textGeometry.get()
+    print 'pass => ' + textPassword.get()
+    print 'fullscreen => ' + str(varFs.get())
+    print 'grabkeyboard => ' + str(varGrabKeyboard.get())
+    print 'homeshare => ' + str(varHomeShare.get())
 
 try:
-    conf = open(configfile,"r")
+    conf = open(configfile, 'r')
 except IOError:
-    popup_alert("No ~/.rdesktop-open found",
-                "No ~/.rdesktop-open file was found.\nChoose 'Save' from the 'File' menu to create one")
+    popup_alert('''No ~/.rdesktop-open found',
+                   No ~/.rdesktop-open file was found.\nChoose 'Save' from
+                   the 'File' menu to create one''')
 else:
-    if(conf):
+    if conf:
         readconf = string.strip(conf.readline())
-        # host, user name, resolution, fullscreen (0,1), grab keyboard (0,1), homeshare (0,1)
+        # host, user name, resolution, fullscreen (0,1), grab keyboard (0,1),
+        # homeshare (0,1)
         optindex = 0
-        for opt in string.split(readconf,","):
-            print opt
+        for opt in string.split(readconf, ','):
             options[optlist[optindex]] = opt
             optindex = optindex + 1
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     root = Tk(className='rdesktop-opener')
-    root.title("rdesktop-opener")
+    root.title('rdesktop-opener')
 
-    menuFrameTop = Frame(root,relief=RIDGE,bd=2)
-    menuFrameTop.pack(side=TOP,fill=X)
+    menuFrameTop = Frame(root, relief=RIDGE,bd=2)
+    menuFrameTop.pack(side=TOP, fill=X)
 
-    menuFileButton = Menubutton(menuFrameTop,text='File',underline=0)
+    menuFileButton = Menubutton(menuFrameTop, text='File', underline=0)
     menuFileButton.pack(side=LEFT)
-    menuFileDropdown = Menu(menuFileButton,relief=RIDGE)
-    menuFileDropdown.add_command(label="Save Current Configuration",underline=0,command=save_conf)
-    menuFileDropdown.add_command(label="Exit",underline=0,command=root.destroy)
+    menuFileDropdown = Menu(menuFileButton, relief=RIDGE)
+    menuFileDropdown.add_command(
+        label='Save Current Configuration', underline=0, command=save_conf)
+    menuFileDropdown.add_command(label='Exit', underline=0, command=root.destroy)
     menuFileButton.config(menu=menuFileDropdown)
 
-    menuOptsButton = Menubutton(menuFrameTop,text='Options',underline=0)
+    menuOptsButton = Menubutton(menuFrameTop, text='Options', underline=0)
     menuOptsButton.pack(side=LEFT)
-    menuOptsDropdown = Menu(menuOptsButton,relief=RIDGE)
-    menuOptsDropdown.add_command(label="Print Debugging Info To Console",underline=0,command=print_options)
+    menuOptsDropdown = Menu(menuOptsButton, relief=RIDGE)
+    menuOptsDropdown.add_command(
+        label='Print Debugging Info To Console', underline=0,
+        command=print_options)
     menuOptsButton.config(menu=menuOptsDropdown)
 
-    menuHelpButton = Menubutton(menuFrameTop,text='Help',underline=0)
+    menuHelpButton = Menubutton(menuFrameTop, text='Help', underline=0)
     menuHelpButton.pack(side=RIGHT)
-    menuHelpDropdown = Menu(menuHelpButton,relief=RIDGE)
-    menuHelpDropdown.add_command(label="RDesktop Homepage",underline=0,command=open_rdesktop_site)
-    menuHelpDropdown.add_command(label="rdesktop-open Homepage",underline=9,command=open_rdo_site)
-    menuHelpDropdown.add_command(label="rdesktop-opener GitHub",underline=9,command=open_rdoer_site)
+    menuHelpDropdown = Menu(menuHelpButton, relief=RIDGE)
+    menuHelpDropdown.add_command(
+        label='RDesktop Homepage', underline=0, command=open_rdesktop_site)
+    menuHelpDropdown.add_command(
+        label='rdesktop-open Homepage', underline=9, command=open_rdo_site)
+    menuHelpDropdown.add_command(
+        label='rdesktop-opener GitHub',underline=9, command=open_rdoer_site)
     menuHelpButton.config(menu=menuHelpDropdown)
 
     # Create the area for hosts
     frameHost = Frame(root)
-    labelHost = Label(frameHost,width=9,text='Host')
+    labelHost = Label(frameHost, width=9, text='Host')
     textHost = Entry(frameHost)
-    textHost.insert(0,options['host'])
+    textHost.insert(0, options['host'])
     frameHost.pack(side=TOP, fill=X)
     labelHost.pack(side=LEFT)
     textHost.pack(side=RIGHT, expand=YES, fill=X)
@@ -153,27 +162,27 @@ if __name__ == "__main__":
 
     # Create the area for user name
     frameUsername = Frame(root)
-    labelUsername = Label(frameUsername,width=9,text='Username')
+    labelUsername = Label(frameUsername, width=9, text='Username')
     textUsername = Entry(frameUsername)
-    textUsername.insert(0,options['user'])
+    textUsername.insert(0, options['user'])
     frameUsername.pack(side=TOP, fill=X)
     labelUsername.pack(side=LEFT)
     textUsername.pack(side=RIGHT, expand=YES, fill=X)
 
     # Create the area for the password
     framePassword = Frame(root)
-    labelPassword = Label(framePassword,width=9,text='Password')
+    labelPassword = Label(framePassword, width=9, text='Password')
     textPassword = Entry(framePassword, show='**')
-    textPassword.insert(0,options['pass'])
+    textPassword.insert(0, options['pass'])
     framePassword.pack(side=TOP, fill=X)
     labelPassword.pack(side=LEFT)
     textPassword.pack(side=RIGHT, expand=YES, fill=X)
 
     # Create the area for geometry
     frameGeometry = Frame(root)
-    labelGeometry = Label(frameGeometry,width=9,text='Geometry')
+    labelGeometry = Label(frameGeometry, width=9, text='Geometry')
     textGeometry = Entry(frameGeometry, width=9)
-    textGeometry.insert(0,options['resolution'])
+    textGeometry.insert(0, options['resolution'])
     frameGeometry.pack(side=TOP, fill=X)
     labelGeometry.pack(side=LEFT)
     textGeometry.pack(side=LEFT, expand=NO)
@@ -182,21 +191,30 @@ if __name__ == "__main__":
     rightFrame = Frame(root)
     varFs = IntVar()
     varFs.set(options['fullscreen'])
-    Checkbutton(rightFrame, text='Fullscreen', variable=varFs).pack(side=RIGHT, expand=YES, fill=X)
+    Checkbutton(
+        rightFrame, text='Fullscreen', variable=varFs).pack(side=RIGHT,
+        expand=YES, fill=X)
     varGrabKeyboard = IntVar()
     varGrabKeyboard.set(options['grabkeyboard'])
-    Checkbutton(rightFrame, text='Grab keyboard', variable=varGrabKeyboard).pack(side=RIGHT, expand=YES, fill=X)
+    Checkbutton(
+        rightFrame, text='Grab keyboard',
+        variable=varGrabKeyboard).pack(side=RIGHT, expand=YES, fill=X)
     varHomeShare = IntVar()
     varHomeShare.set(options['homeshare'])
-    Checkbutton(rightFrame, text='Share Home Dir', variable=varHomeShare).pack(side=RIGHT, expand=YES, fill=X)
+    Checkbutton(
+        rightFrame, text='Share Home Dir',
+        variable=varHomeShare).pack(side=RIGHT, expand=YES, fill=X)
     rightFrame.pack(side=TOP)
-
 
     frameButtons = Frame(root)
     frameButtons.pack(side=BOTTOM)
-    Button(frameButtons,text="Quit",command=root.destroy).pack(side=LEFT, fill=X)
-    Button(frameButtons,text='Connect',command=run_rdesktop).pack(side=RIGHT, fill=X)
+    Button(
+        frameButtons, text='Quit', command=root.destroy).pack(side=LEFT,
+        fill=X)
+    Button(
+        frameButtons, text='Connect', command=run_rdesktop).pack(side=RIGHT,
+        fill=X)
 
     root.mainloop()
 
-print "rdesktop-opener.py -- https://github.com/robled/rdesktop-opener"
+print 'rdesktop-opener.py -- https://github.com/robled/rdesktop-opener'
