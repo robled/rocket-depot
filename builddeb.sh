485f23645ff2572d20e03c276a8835edf5f5d35f
robled <robled@gmail.com>
2013-09-26 14:44:10 -0400

    more automation for the build script

--- builddeb.sh
+++ builddeb.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+littlev=$(grep Debian-Version stdeb.cfg | awk {'print $2'})
+bigv=$(grep version setup.py | awk {'print $3'} | cut -c 2-4)
+echo $littlev
+echo $bigv
+
 if [ -d "deb_dist" ] && [ -d "rocket_depot.egg-info" ]; then
     echo 'cleaning previous build'
     rm -rf deb_dist rocket_depot.egg-info
@@ -7,8 +12,11 @@ fi
 
 python setup.py --command-packages=stdeb.command sdist_dsc
 cd deb_dist
-dpkg-source -x rocket-depot_0.1-1ppa1-1.dsc
-cd rocket-depot-0.1-1ppa1
+dpkg-source -x rocket-depot_$bigv-$littlev.dsc
+cd rocket-depot-$bigv
 debuild -S -sa
 
-# dput ppa:robled/rocket-depot deb_dist/rocket-depot_0.1-1_source.changes
+echo 
+echo 'To upload to PPA, copypasta:'
+echo "dput ppa:robled/rocket-depot deb_dist/rocket-depot_"$bigv"-"$littlev"_source.changes"
+
