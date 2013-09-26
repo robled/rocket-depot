8feeb69c4aa365e1168c7843251bf30909a27864
robled <robled@gmail.com>
2013-09-26 17:01:17 -0400

    added new stuff for pypi builds

--- builddeb.sh
+++ builddeb.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 littlev=$(grep Debian-Version stdeb.cfg | awk {'print $2'})
 bigv=$(grep version setup.py | awk {'print $3'} | cut -c 2-4)
+cleanup=('deb_dist' 'rocket_depot.egg-info' 'dist')
 
-if [ -d "deb_dist" ] && [ -d "rocket_depot.egg-info" ]; then
-    echo 'cleaning previous build'
-    rm -rf deb_dist rocket_depot.egg-info
-fi
+for i in "${cleanup[@]}"
+do
+    echo "cleaning $i"
+    rm -rf $i
+done
 
 python setup.py --command-packages=stdeb.command sdist_dsc
 cd deb_dist
@@ -15,5 +17,6 @@ cd rocket-depot-$bigv
 debuild -S -sa
 
 echo
-echo 'To upload to PPA, copypasta:'
+echo 'To upload to PPA/pypi, copypasta:'
 echo "dput ppa:robled/rocket-depot deb_dist/rocket-depot_"$bigv"-"$littlev"_source.changes"
+echo 'python setup.py sdist upload'
