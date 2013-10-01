d305af2f562d2cbb76270cfc2948c83af91b4586
robled <robled@gmail.com>
2013-10-01 17:53:53 -0400

    new version number 0.11 added

--- rocket-depot.py
+++ rocket-depot.py
@@ -495,7 +495,8 @@ class MainWindow(Gtk.Window):
     def on_about(self, widget):
         about = Gtk.AboutDialog()
         about.set_program_name("Rocket Depot")
-        about.set_version("0.1")
+        about.set_version("0.11")
+        about.set_copyright("2013 David Roble")
         about.set_comments("rdesktop/xfreerdp Frontend")
         about.set_website("https://github.com/robled/rocket-depot")
         about.run()
