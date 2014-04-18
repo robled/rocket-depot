f8aff37c60769e8e1b67c55c10618a71ff7c7823
robled <robled@gmail.com>
2014-04-18 16:37:29 -0400

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ shutil.copyfile('rocket-depot.py', 'rocket-depot')
 
 setup(
     name = 'rocket-depot',
-    version = '0.21',
+    version = '0.22',
     scripts = ['rocket-depot'],
 
     # metadata for upload to PyPI
