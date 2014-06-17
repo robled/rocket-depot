99b97c778257ae6daa6c31fbbd78a608b822f16d
robled <robled@gmail.com>
2014-06-17 17:47:24 -0400

    version bump

--- rocket-depot.py
+++ rocket-depot.py
@@ -675,7 +675,7 @@ Useful for diagnosing connection problems''')
     def on_about(self, widget):
         about = Gtk.AboutDialog()
         about.set_program_name("Rocket Depot")
-        about.set_version("0.23")
+        about.set_version("0.24")
         about.set_copyright("2014 David Roble")
         about.set_comments("rdesktop/xfreerdp Frontend")
         about.set_website("https://github.com/robled/rocket-depot")
